39450ab1f74eb43e1b9ddf4135a1342852e0a07a
thierry SAUTIERE <tsautiere@aol.com>
2023-06-23 18:35:51 +0000

    structure première

---
+++ src/structures/premiere_spe.js
@@ -0,0 +1,117 @@
+export default  {
+    "Développement": {
+        "image": 2,
+        "thèmes fiche": [['Développement', "pdf/seconde/developpements/Developpement.pdf"]],
+        "thèmes ligne": [["Double distributivité", "seconde/developpement/double_distributivite/Double distributivite.html"], 
+        ["Identité remarquables", "seconde/developpement/identites_remarquables/Identitesremarquables.html"]]
+    },
+    'Equations de droites': {
+        "image": 3,
+        "thèmes fiche": [["Equations cartésiennes de droites", "pdf/seconde/equationsDroites/Equations_cartesiennes_de_droites.pdf"]],
+        "thèmes ligne": [["Equations de droites(calculs)", "seconde/equationsDroites/calculs/Eqsdtescalculs.html"]]
+    },
+    'Equations': {
+        "image": 4,
+        "thèmes fiche": [["Equations", "pdf/seconde/equations/Equations_diverses.pdf"],
+        ["Equations de degré 2", "pdf/seconde/equations/Equations_de_degre_2.pdf"]
+        ],
+        "thèmes ligne": [["Equations avec factorisation", "#"],
+        ["Equations produits", "#"],
+        ["Equations quotient", "#"],
+        ["Equations de degré 1", "#"],
+        ["Equations de degré 2", "#"]
+        ]
+    }, 'Factorisation': {
+        "image": 5,
+        "thèmes fiche": [["Factorisation", "pdf/seconde/factorisation/Factorisation.pdf"]],
+        "thèmes ligne": [["#", "#"]]
+    },
+    'Fonction': {
+        "image": 6,
+        "thèmes fiche": [["Images antécédents", "pdf/seconde/fonctions/Images_antecedents_equations_inequations_graphiques.pdf"],
+        ["Hyperboles", "pdf/seconde/fonctions/Hyperboles_et_questions.pdf"],
+        ["Paraboles", "pdf/seconde/fonctions/Paraboles_et_questions.pdf"],
+        ["Parité(graphique)", "pdf/seconde/fonctions/Parite_(aspect_graphique).pdf"],
+        ["Parité(calculs)", "pdf/seconde/fonctions/Parite_(par_calculs).pdf"],
+        ["Sens de variations de fonctions(calculs)", "pdf/seconde/fonctions/Sens_de_variations_de_fonctions_(par_calculs).pdf"],
+        ["Variations de fonctions(calculs)", "pdf/seconde/fonctions/Variations_de_fonctions_(par_calculs).pdf"],
+        ["Tableaux de variations(questions)", ["pdf/seconde/fonctions/Tableaux_de_variations_et_questions.pdf"]],
+        ["Tableaux de valeurs de courbes", "pdf/seconde/fonctions/Tableaux_de_valeurs_courbes_et_questions.pdf"],
+        ["Tableaux de variations(graphiques)", "pdf/seconde/fonctions/Tableaux_de_variations_(avec_graphiques).pdf"],
+        ["Tableaux de variations(équations|inéquations)", "pdf/seconde/fonctions/Tableaux_de_variations_equations_et_inequations.pdf"],
+
+        ],
+        "thèmes ligne": [["Images et antécédents", "seconde/fonctions/antecedents_images/ImagesAntecedents.html"]]
+    },
+    'Inéquations': {
+        'image': 7,
+        "thèmes fiche": [["Inéquation(divers)", "pdf/seconde/inequations/Inequations_(divers).pdf"],
+        ["Inéquations(tableaux de signes de base)", "pdf/seconde/inequations/Inequations_(tableaux_de_signes_de_base).pdf"],
+        ["Inéquations(tableaux de signes divers)", "pdf/seconde/inequations/Inequations_(tableaux_de_signes_divers).pdf"],
+        ["Inéquations simples(intersections | réunions)", "pdf/seconde/inequations/Inequations_simples_intersections_reunions.pdf"],
+        ["Systèmes d'inéquations", "pdf/seconde/inequations/Systemes_d_inequations.pdf"],
+        ],
+        "thèmes ligne": [["#", "#"]]
+    },
+    'Milieu et longueur': {
+        'image': 8,
+        "thèmes fiche": [["Coordonnées|projeté orthogonal|trigonométrie", "pdf/seconde/milieuLongueur/Coordonnees_projete_orthogonal_trigonometrie.pdf"],
+        ["Milieu et longueur(calculs de base)", "pdf/seconde/milieuLongueur/Milieu_longueur_calculs_de_base.pdf"],
+        ["Milieu et longueur(divers)", "pdf/seconde/milieuLongueur/Milieu_longueur.pdf"],
+        ["Points alignés(longueurs)", "pdf/seconde/milieuLongueur/Points_alignes_avec_les_longueurs.pdf"],
+        ],
+        "thèmes ligne": [["#", "#"]],
+    },
+    'Nombres et calculs': {
+        'image': 9,
+        "thèmes fiche": [["Décomposition en facteurs premiers", "pdf/seconde/nombresCalculs/Decomposition_en_facteurs_premiers.pdf"],
+        ["Fractions", "pdf/seconde/nombresCalculs/Fractions.pdf"],
+        ["Puissances", "pdf/seconde/nombresCalculs/Puissances.pdf"],
+        ["Racines", "pdf/seconde/nombresCalculs/Racines.pdf"],
+        ],
+        "thèmes ligne": [["#", "#"]],
+    },
+    'Pourcentages': {
+        'image': 10,
+        "thèmes fiche": [["Pourcentages(fiche 1)", "pdf/seconde/pourcentages/Pourcentages_fiche_1.pdf"],
+        ["Pourcentages(fiche 2)", "pdf/seconde/pourcentages/Pourcentages_fiche_2.pdf"]
+        ],
+        "thèmes ligne": [["#", "#"]]
+    },
+    'Probabilités': {
+        "image": 11,
+        "thèmes fiche": [["Diagrammes de Venn", "pdf/seconde/probabilites/Diagrammes_de_Venn.pdf"],
+        ["Probabilités(divers)", "pdf/seconde/probabilites/Probabilites_(divers).pdf"],
+        ["Probabilités et arbres", "pdf/seconde/probabilites/Probabilites_et_arbres.pdf"],
+        ["Probabilités et tableaux", "pdf/seconde/probabilites/Probabilites_et_tableaux.pdf"]
+        ],
+        "thèmes ligne": [["#", "#"]]
+    },
+    'Statistiques': {
+        "image": 12,
+        "thèmes fiche": [["Statistiques", "pdf/seconde/statistiques/Statistiques.pdf"]],
+        "thèmes ligne": [["#", "#"]]
+    },
+    'Systèmes': {
+        "image": 13,
+        "thèmes fiche": [["Systèmes(calculs)", "pdf/seconde/systemes/Systemes_(calculs).pdf"],
+        ["Systèmes(graphiques)", "pdf/seconde/systemes/Systemes_(graphiques).pdf"]
+        ],
+        "thèmes ligne": [["Systèmes(faciles)", "seconde/systemes/systemes_faciles/Systemes(tombe_juste).html"],
+                            ]
+    },
+    'Valeur absolue': {
+        "image": 13,
+        "thèmes fiche": [["Valeur absolue(équations|inéquations)", "pdf/seconde/valeurAbsolue/Valeur_absolue(équations_inéquations).pdf"]],
+        "thèmes ligne": [["Équations", "seconde/valeurAbsolue/equations/EquationsValAbs.html"],
+        ["Inéquations", "seconde/valeurAbsolue/inequations/InequationsValAbs.html"]
+    ]
+    },
+    'Vecteurs': {
+        "image": 1,
+        "thèmes fiche": [["Constructions de vecteurs", "pdf/seconde/vecteurs/Constructions_de_vecteurs.pdf"],
+        ["Vecteurs(calculs de base)", "pdf/seconde/vecteurs/Vecteurs_(calculs_de_base).pdf"],
+        ["Vecteurs(coordonnées)", "pdf/seconde/vecteurs/Vecteurs_(coordonnees).pdf"]],
+        "thèmes ligne": [["#", "#"]]
+    },
+}
