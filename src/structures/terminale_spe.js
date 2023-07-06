448ad6cd5d8edf7ae3365c43720f05ef54a448ae
thierry SAUTIERE <tsautiere@aol.com>
2023-07-06 11:17:53 +0000

    Numworks mise à jour

---
+++ src/structures/terminale_spe.js
@@ -0,0 +1,7 @@
+export default {
+    "Equations différentielles":{
+        "image": 1,
+        "thèmes fiche": [['Equations différencielles', "pdf/terminale_spe/developpements/EquationsDifferencielles/Equationsdifferentielles.pdf"]],
+        "thèmes ligne": []
+    }
+}
