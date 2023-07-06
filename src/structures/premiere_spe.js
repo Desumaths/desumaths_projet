8f0b72a98dbf240ce9e937276c3695abd4c63414
Desumaths <137373337+Desumaths@users.noreply.github.com>
2023-07-06 12:23:33 +0000

    correction titre degré2

--- src/structures/premiere_spe.js
+++ src/structures/premiere_spe.js
@@ -8,7 +8,7 @@ export default {
         "image": 1,
         "thèmes fiche": [['#', '#']],
         "thèmes ligne": [
-            ["Equations Simples", "premiere_spe/SecondDegre/Equations/TombeJuste/EqsDeg2TombeJuste.html"],
+            ["Equations avec solutions entières", "premiere_spe/SecondDegre/Equations/TombeJuste/EqsDeg2TombeJuste.html"],
             ["Equations quelconques", "premiere_spe/SecondDegre/Equations/Quelconque/EqsDeg2.html"],
             ["Forme canonique", "premiere_spe/SecondDegre/FormeCanonique/FormeCanonique.html"]
         ]
