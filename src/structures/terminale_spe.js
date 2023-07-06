11cebe8790e7909dcc4d5e4fa1b012b5fe9de3cc
thierry SAUTIERE <tsautiere@aol.com>
2023-07-06 11:40:53 +0000

    Terminale SPE Fiches PDF

--- src/structures/terminale_spe.js
+++ src/structures/terminale_spe.js
@@ -1,7 +1,86 @@
 export default {
     "Equations différentielles":{
         "image": 1,
-        "thèmes fiche": [['Equations différencielles', "pdf/terminale_spe/developpements/EquationsDifferencielles/Equationsdifferentielles.pdf"]],
+        "thèmes fiche": [['Equations différencielles', "pdf/terminale_spe/EquationsDifferencielles/Equations_differentielles.pdf"]],
         "thèmes ligne": []
-    }
+    },
+    "Espace":{
+        "image": 2,
+        "thèmes fiche": [
+            ["Projeté orthogonal d'un point sur une droite", "pdf/terminale_spe/espace/Projeteorthogonaldunpointsurunedroite.pdf"],
+            ["Projeté orthogonal d'un point sur un plan", "pdf/terminale_spe/espace/Projeteorthogonaldunpointsurunplan.pdf"],
+            ['Révisions', "pdf/terminale_spe/espace/Revisionsespace.pdf"],
+            ['Savoir faire', "pdf/terminale_spe/espace/Savoirfaireespace.pdf"]
+        ],
+        "thèmes ligne": []
+    },
+    "Intégration":{
+        "image": 3,
+        "thèmes fiche": [
+            ["Intégration par partie", "pdf/terminale_spe/integrales/Integrationparparties.pdf"],
+        ],
+        "thèmes ligne": []
+    },
+    "Limites":{
+        "image": 3,
+        "thèmes fiche": [
+            ["Changement de variable", "pdf/terminale_spe/limites/Limites(changementdevariable).pdf"],
+            ["Comparaison", "pdf/terminale_spe/limites/Limites(comparaison).pdf"],
+            ["Fonctions composées", "pdf/terminale_spe/limites/Limites(fonctionscomposees).pdf"]
+        ],
+        "thèmes ligne": []
+    },
+    "Logarithme":{
+        "image": 4,
+        "thèmes fiche": [
+            ["Equations|Inéquations", "pdf/terminale_spe/ln/Equations_inequations.pdf"],
+            ["Amérique du nord(mai 2021)", "pdf/terminale_spe/ln/ExercicelnAmeriqueduNordmai2021.pdf"],
+            ["Asie(juin 2021| sujet 1)", "pdf/terminale_spe/ln/ExercicelnAsiejuin2021.pdf"],
+            ["Asie(juin 2021| sujet 2)", "pdf/terminale_spe/ln/ExercicelnAsiejuin2021(sujet 2).pdf"],
+            ["Métropole(mars 2021| sujet 1)", "pdf/terminale_spe/ln/ExercicelnMetropolemars2021(sujet 1).pdf"],
+            ["Métropole(mars 2021| sujet 2)", "pdf/terminale_spe/ln/ExercicelnMetropolemars2021(sujet 2).pdf"],
+            ["Polynésie(juin 2021)", "pdf/terminale_spe/ln/ExercicelnPolynesiejuin2021.pdf"],
+            ["Sujet 0(2021)", "pdf/terminale_spe/ln/Exercicelnsujet0_2021.pdf"],
+        ],
+        "thèmes ligne": []
+    },
+    "Primitives":{
+        "image": 5,
+        "thèmes fiche": [
+            ["Primitives 1", "pdf/terminale_spe/primitives/Primitives.pdf"],
+            ["Primitives 2", "pdf/terminale_spe/primitives/Primitives(2).pdf"],
+        ],
+        "thèmes ligne": []
+    },
+    "Probabilités":{
+        "image": 6,
+        "thèmes fiche": [
+            ["Probabilités", "pdf/terminale_spe/probabilites/Probabilites.pdf"],
+            ["Loi binomiale", "pdf/terminale_spe/probabilites/Loibinomiale.pdf"],
+            ["Conditionnement", "pdf/terminale_spe/probabilites/Probabilites(conditionnement).pdf"],
+        ],
+        "thèmes ligne": []
+    },
+    "Suites":{
+        "image": 7,
+        "thèmes fiche": [
+            ["Rappels", "pdf/terminale_spe/suites/Suites(rappelspremiere).pdf"],
+            ["Récurrence", "pdf/terminale_spe/suites/Recurrence.pdf"],
+        ],
+        "thèmes ligne": []
+    },
+    "Trigonométrie":{
+        "image": 8,
+        "thèmes fiche": [
+            ["Equations|Inéquations", "pdf/terminale_spe/trigonometrie/Trigonometrie(equationsetinequations).pdf"],
+        ],
+        "thèmes ligne": []
+    },
+    "TVI":{
+        "image": 9,
+        "thèmes fiche": [
+            ["TVI", "pdf/terminale_spe/tvi/TVI.pdf"],
+        ],
+        "thèmes ligne": []
+    },
 }
